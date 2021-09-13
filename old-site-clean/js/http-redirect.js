08c0d6dbee6e61e080aa0cd1fe1220706f481f86
Jacopo <jacoping@gmail.com>
2021-09-13 17:26:58 +0200

    cleanup

--- old-site-clean/js/http-redirect.js
+++
@@ -1,5 +0,0 @@
-  // redirect from http to https
-  var host = "vuoto.xyz"
-  if (window.location.host == host && window.location.protocol != "https:") {
-    window.location.protocol = "https:"
-  }
